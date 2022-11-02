bf022088f8507cbc4551d338ad9a5bcfa3b02425
Vinnie Gray <unbansheee@gmail.com>
2022-11-02 16:34:28 +1300

    displaying some data on user page

--- Assets/UserInfoPage.cs
+++ Assets/UserInfoPage.cs
@@ -10,7 +10,7 @@ public class UserInfoPage : MonoBehaviour
     
     public NFTGenerator pageIcon;
     public TextMeshProUGUI name;
-    
+    public TextMeshProUGUI data;
     
     // Start is called before the first frame update
     void Start()
@@ -23,15 +23,32 @@ public class UserInfoPage : MonoBehaviour
         gameObject.SetActive(false);
         
     }
-    
-    
+
+
     public void Open(User user, NFTGenerator icon)
     {
         userData = user;
         originalIcon = icon;
-        
+        data.text = "";
+
         pageIcon.Set(originalIcon);
+        
         name.text = userData.name;
+
+        
+        data.text += "Email: " + userData.email + "\n";
+        data.text += "Phone: " + userData.phoneNumber + "\n";
+        data.text += "Age: " + userData.age + " years \n";
+        
+        data.text += "Identity: " + userData.genderIdentity + "\n";
+        
+        data.text += "Education: " + userData.education + "\n";
+        data.text += "Occupation: " + userData.occupation + "\n";
+        data.text += "Years of Experience: " + userData.experience + " years \n";
+        data.text += "Salary: " + userData.salary + " USD \n";
+        
+        
+        
         
         gameObject.SetActive(true);
     }
