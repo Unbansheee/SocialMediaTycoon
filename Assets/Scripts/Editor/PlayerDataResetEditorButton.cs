0292228da5b0c88e84ec612461efa46bae09058b
Vinnie Gray <unbansheee@gmail.com>
2022-11-12 23:04:20 +1300

    Dynamic number of users on user page

---
+++ Assets/Scripts/Editor/PlayerDataResetEditorButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlayerData))]
+public class PlayerDataResetEditorButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var  myScript = (PlayerData)target;
+        if (GUILayout.Button("Reset"))
+        {
+            myScript.Reset();
+        }
+        if (GUILayout.Button("Buff Mode"))
+        {
+            myScript.Money = 1000000;
+            myScript.SiteUsers = 1000000;
+            myScript.DataMB = 1000000;
+            myScript.Reputation = 1000000;
+            myScript.SiteUsersPerSecond = 100;
+            myScript.DataMBPerSecond = 100;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ProfilePage.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfilePage : MonoBehaviour
+{
+    public GameObject personGrid;
+    public GameObject personPrefab;
+    
+    public UserInfoPage userInfoPage;
+    public PlayerData playerData;
+    
+    public void Refresh()
+    {
+        
+        var users = playerData.SiteUsers;
+        int usersCount = Math.Clamp(users, 1, 30);
+
+        while (personGrid.transform.childCount < usersCount)
+        {
+            var person = Instantiate(personPrefab, personGrid.transform);
+            var personScript = person.GetComponent<UI_PersonIcon>();
+            personScript.infoPageRef = userInfoPage;
+            personScript.GenerateWidget();
+        }
+        
+        if (personGrid.transform.childCount > usersCount)
+        {
+            for (int i = personGrid.transform.childCount - 1; i >= usersCount; i--)
+            {
+                Destroy(personGrid.transform.GetChild(i).gameObject);
+            }
+        }
+        
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in personGrid.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        Refresh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dont do this except i will do this because im a bad programmer :)
+        Refresh();
+    }
+}
