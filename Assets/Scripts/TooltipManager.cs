94bd10dc4b6049871924925d38e3911ab2d2003d
Vinnie Gray <unbansheee@gmail.com>
2022-11-13 23:15:20 +1300

    Fixed tooltip flickering, toolbar occlusion, and moving off the screen

--- Assets/Scripts/TooltipManager.cs
+++ Assets/Scripts/TooltipManager.cs
@@ -7,6 +7,7 @@ public class TooltipManager : MonoBehaviour
 {
     public static TooltipManager _instance;
     public TextMeshProUGUI textComponent;
+    public RectTransform bounds;
 
     // make sure there is only ever one tooltip manager instance
     private void Awake()
@@ -31,6 +32,25 @@ public class TooltipManager : MonoBehaviour
     void FixedUpdate()
     {
         transform.position = Input.mousePosition + new Vector3(-(3), (transform.GetComponent<RectTransform>().rect.height/2) + 3, 0);
+        // if RectTransform overlap the edge of the screen, move the tooltip
+        if (transform.position.x + (bounds.rect.width * 0.5) > Screen.width)
+        {
+            transform.position = new Vector3(Screen.width - (bounds.rect.width * 0.5f), transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x - (bounds.rect.width * 0.5) < 0)
+        {
+            transform.position = new Vector3((bounds.rect.width * 0.5f), transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y + bounds.rect.height > Screen.height + 32.0f)
+        {
+            transform.position = new Vector3(transform.position.x, Screen.height + 32.0f - bounds.rect.height, transform.position.z);
+        }
+        
+        
+        
+        
+
     }
 
     public void SetAndShowToolTip(string message)
