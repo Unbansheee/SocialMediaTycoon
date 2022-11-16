fe58d2d4b2d29391bc02910965c5c43a0a614ac6
Vinnie Gray <unbansheee@gmail.com>
2022-11-16 18:35:42 +1300

    reset button

---
+++ Assets/Reset.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetScene()
+    {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            
+    }
+}
